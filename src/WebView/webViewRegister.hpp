9049668eadfba5359dcef67b24d432209a44034d
Daniil Ubica <acveloff@gmail.com>
2026-05-19 18:49:46 +0300

    webEngine added

--- src/WebView/webViewRegister.hpp
+++ src/WebView/webViewRegister.hpp
@@ -1,5 +1,7 @@
 #ifdef __APPLE__
     #include "macOS/webView.h"
+#elif defined(Q_OS_LINUX) || defined(Q_OS_WIN)
+    #include <QtWebEngineQuick/QtWebEngineQuick>
 #endif
 
 namespace QSiesta::Internal {
@@ -8,6 +10,8 @@ namespace webViewRegister {
     static void registerPlatformType() {
         #ifdef __APPLE__
             qmlRegisterType<WebView>("qsiesta.plugin.types", 1, 0, "WKWebView");
+        #elif defined(Q_OS_LINUX) || defined(Q_OS_WIN)
+            QtWebEngineQuick::initialize();
         #endif
     }
 
